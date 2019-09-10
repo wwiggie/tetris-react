f460b8e5f841ea4e4de31dd06e115f983889d754
Wendy Wigg <wiggwendy@gmail.com>
2019-09-10 13:51:35 -0700

    add game over state

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { createStage } from "../gameHelpers";
-import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
+// Styled Components
+import { StyledTetrisWrapper, StyledTetris } from "./styles/StyledTetris";
+
+// Custom Hooks
+import { usePlayer } from "../hooks/usePlayer";
+import { useStage } from "../hooks/useStage";
 
 // Components
 import Stage from "./Stage";
@@ -9,18 +13,30 @@ import Display from "./Display";
 import StartButton from "./StartButton";
 
 const Tetris = () => {
+  const [dropTime, setDropTime] = useState(null);
+  const [gameOver, setGameOver] = useState(false);
+
+  const [player] = usePlayer();
+  const [stage, setStage] = useStage(player);
+
+  console.log("re-render");
+
   return (
     <StyledTetrisWrapper>
-        <StyledTetris>
-      <Stage stage={createStage()} />
-      <aside>
-        <div>
-          <Display text="Score" />
-          <Display text="Rows" />
-          <Display text="Level" />
-        </div>
-        <StartButton />
-      </aside>
+      <StyledTetris>
+        <Stage stage={stage} />
+        <aside>
+          {gameOver ? (
+            <Display gameOver={gameOver} text="Game Over" />
+          ) : (
+            <div>
+              <Display text="Score" />
+              <Display text="Rows" />
+              <Display text="Level" />
+            </div>
+          )}
+          <StartButton />
+        </aside>
       </StyledTetris>
     </StyledTetrisWrapper>
   );
--- src/tetrominos.js
+++ src/tetrominos.js
@@ -61,6 +61,6 @@ export const TETROMINOS = {
 export const randomTetromino = () => {
     const tetrominos = 'IJLOSTZ';
     const randTetromino = 
-        tetrominos[matchMedia.floor(Math.random() * tetrominos.length)];
+        tetrominos[Math.floor(Math.random() * tetrominos.length)];
     return TETROMINOS[randTetromino];
 }
