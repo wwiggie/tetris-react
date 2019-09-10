bb38fc6d4fc57e2b191d1dca37b6e1a04e3d36f9
Wendy Wigg <wiggwendy@gmail.com>
2019-09-10 13:25:42 -0700

    added styles

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,28 +1,29 @@
-import React from 'react';
+import React from "react";
 
-import { createStage } from '../gameHelpers';
+import { createStage } from "../gameHelpers";
+import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
 
 // Components
-import Stage from './Stage';
-import Display from './Display';
-import StartButton from './StartButton';
+import Stage from "./Stage";
+import Display from "./Display";
+import StartButton from "./StartButton";
 
 const Tetris = () => {
-
-
-    return (
+  return (
+    <StyledTetrisWrapper>
+        <StyledTetris>
+      <Stage stage={createStage()} />
+      <aside>
         <div>
-            <Stage stage={createStage()} />
-            <aside>
-                <div>
-                <Display text="Score" />
-                <Display text="Rows" />
-                <Display text="Level" />
-                </div>
-                <StartButton />
-            </aside>
+          <Display text="Score" />
+          <Display text="Rows" />
+          <Display text="Level" />
         </div>
-    );
+        <StartButton />
+      </aside>
+      </StyledTetris>
+    </StyledTetrisWrapper>
+  );
 };
 
 export default Tetris;
