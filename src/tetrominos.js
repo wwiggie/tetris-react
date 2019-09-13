e62d863422bb2fe4e688596e6a1c4c4c351a2ec7
Wendy Wigg <wiggwendy@gmail.com>
2019-09-13 15:21:15 -0700

    tetrominos show and movement working

--- src/tetrominos.js
+++ src/tetrominos.js
@@ -51,7 +51,7 @@ export const TETROMINOS = {
     Z: {
         shape: [
             ['Z', 'Z', 0],
-            [0, 'Z', 0],
+            [0, 'Z', 'Z'],
             [0, 0, 0]
         ],
         color: '227, 78, 78',
