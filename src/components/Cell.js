f0d093b78a1a599b4f68b3d51926200ddb0316ba
Wendy Wigg <wiggwendy@gmail.com>
2019-09-15 10:27:39 -0700

    working scores & levels

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import { StyledCell } from './styles/StyledCell';
-import { TETROMINOS } from '../tetrominos';
+import React from "react";
+import { StyledCell } from "./styles/StyledCell";
+import { TETROMINOS } from "../tetrominos";
 
 const Cell = ({ type }) => (
-    <StyledCell type={type} color={TETROMINOS[type].color} />
-)
+  <StyledCell type={type} color={TETROMINOS[type].color}>
+    {console.log("rerender")}
+  </StyledCell>
+);
 
-export default Cell;
+export default React.memo(Cell);
